36d3b499984fe1c2a8ae55d1cd0bb659daf14217
Mark Fang <mark.fang.mo@gmail.com>
2022-05-12 17:20:28 -0500

    typo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -68,7 +68,7 @@ const Home = () => {
         >
           <path
             fill="#FFFFFF"
-            fill-opacity="1"
+            fillOpacity="1"
             d="M0,192L48,208C96,224,192,256,288,256C384,256,480,224,576,224C672,224,768,256,864,266.7C960,277,1056,267,1152,229.3C1248,192,1344,128,1392,96L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
           ></path>
         </svg>
@@ -112,13 +112,13 @@ const Home = () => {
         id="features"
         className="flex flex-col items-center p-10 space-y-8 bg-white"
       >
-        <h1 className="text-4xl md:text-6xl">
+        <h1 className="text-3xl md:text-6xl">
           Made for{" "}
           <span className="p-2 font-bold bg-yellow-300 rounded-xl ">
             Students.
           </span>
         </h1>
-        <h1 className="text-4xl md:text-6xl">
+        <h1 className="text-3xl md:text-6xl">
           <span className="p-2 font-bold bg-yellow-300 rounded-xl ">
             Connecting
           </span>{" "}
